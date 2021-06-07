58cdf4f9ac39f074c7048169002870816339330c
David Meunier <david_meunier_79@hotmail.fr>
2021-06-07 10:35:19 +0200

    corrected 1/2 all grad

--- nipype_workflows/nodes/function.py
+++ nipype_workflows/nodes/function.py
@@ -91,7 +91,7 @@ def create_mean_acq_files(bval_AP, bval_PA, readout_time):
     acq_param_file = os.path.abspath("mean_acq_parameters")
     acq_index_file = os.path.abspath("mean_acq_index")
 
-    count = 0
+    count = 1
 
     paramf = open(acq_param_file, "w")
     indexf = open(acq_index_file, "w")
@@ -104,22 +104,26 @@ def create_mean_acq_files(bval_AP, bval_PA, readout_time):
         list_bval_new = replace(list_bval, dict_rep)
 
         paramf.write("0 -1 0 {}\n".format(readout_time))
-        count+=1
-        indexf.write("{}\n".format(count))
+
+        for val_B  in list_bval_new:
+            indexf.write("{}\n".format(count))
+
 
     bval_AP_new_file = os.path.abspath("bval_AP_new")
 
     with open(bval_AP_new_file, "w") as f:
         f.write(" ".join(list_bval_new))
 
+    count+=1
+
     with open(bval_PA, "r") as f:
         list_bval = f.readlines()[0].split()
         list_bval_new = replace(list_bval, dict_rep)
 
         paramf.write("0 1 0 {}\n".format(readout_time))
-        count+=1
-        indexf.write("{}\n".format(count))
 
+        for val_B  in list_bval_new:
+            indexf.write("{}\n".format(count))
 
     bval_PA_new_file = os.path.abspath("bval_PA_new")
 
